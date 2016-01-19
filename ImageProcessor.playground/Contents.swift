18cf5c8891b3281acf035a31b3950db4cf4573f4
Gabriela Vazquez <gvazquez@apigee.com>
2016-01-18 20:18:06 -0800

    Initial status of playground

---
+++ ImageProcessor.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let image = UIImage(named: "sample")!
+
+// Process the image!
+let myRGBA = RGBAImage(image: image)!
+
+// implement brightness filter
+// implement contrast filter
+// implement blur filter
+// implement motion blur
+// implement sharpen filter
+// implement grey scale?
